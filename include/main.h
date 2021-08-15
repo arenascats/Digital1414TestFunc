84cfa632b57ea17ab8a2f1078e642a06d5c19d5f
arenascats <wangjinxuan1222@163.com>
2021-08-15 17:11:41 +0800

    second

--- include/main.h
+++ include/main.h
@@ -7,5 +7,5 @@ void CharDis(char character); //
 void DLprint(int i);//字符打印，超过4个字符就滚动显示
 void DLprint(String s);
 void Test();//测试
-void DLWrite(int i6, int i5, int i4, int i3, int i2, int i1, int i0);
+void DLWrite(int i6, int i5, int i4, int i3, int i2, int i1, int i0); //控制引脚以写数据
 void DLrollDigi(int i);
